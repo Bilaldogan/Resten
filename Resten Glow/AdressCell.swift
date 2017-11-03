21e213f038760610def62cdf87f51592f94341bc
bilal <bilaldogan325@gmail.com>
2017-11-03 05:11:11 +0300

    Profilim Sayfası

---
+++ Resten Glow/AdressCell.swift
@@ -0,0 +1,34 @@
+//
+//  AdressCell.swift
+//  Resten Glow
+//
+//  Created by bilal on 03/11/2017.
+//  Copyright © 2017 bilal. All rights reserved.
+//
+
+import UIKit
+
+class AdressCell: UITableViewCell {
+
+    @IBOutlet weak var imageViewIcon: UIImageView!
+    @IBOutlet weak var labelAddressType: UILabel!
+    @IBOutlet weak var labelAddress: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    func configureWithItem(type : AddressCellType) {
+        if type == AddressCellType.creditCard {
+            imageViewIcon.image = #imageLiteral(resourceName: "credit-card-icon")
+            labelAddress.text = "4543 **** **** ****"
+            labelAddressType.text = "Kredi Kartı"
+        }
+    }
+
+    
+}
+enum AddressCellType {
+    case creditCard
+    case address
+    case eft
+}
--- Resten Glow/MyAccountController.swift
+++ Resten Glow/MyAccountController.swift
@@ -15,6 +15,9 @@ class MyAccountController: BaseController {
         super.viewDidLoad()
         tableView.register(UINib(nibName: "SmallProfileCell", bundle: nil), forCellReuseIdentifier: "profileCell")
         tableView.register(UINib(nibName: "AddInfoCell", bundle: nil), forCellReuseIdentifier: "addInfoCell")
+         tableView.register(UINib(nibName: "AdressCell", bundle: nil), forCellReuseIdentifier: "addressCell")
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 140
     }
 
    
@@ -24,9 +27,25 @@ extension MyAccountController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 && indexPath.row == 0 {
             return  tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath) as! SmallProfileCell
-        } else if indexPath.section == 1 && indexPath.row == 0 {
+        } else if indexPath.section == 1 {
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath) as! AdressCell
+                return cell
+            } else if indexPath.row == 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "addInfoCell", for: indexPath) as! AddInfoCell
             return cell
+            }
+        } else if indexPath.section == 2 {
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath) as! AdressCell
+                cell.configureWithItem(type: .creditCard)
+                return cell
+            } else if indexPath.row == 1 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "addInfoCell", for: indexPath) as! AddInfoCell
+                cell.label.text = "ÖDEME YÖNTEMİ EKLE"
+                return cell
+            }
+        
         }
         return UITableViewCell()
     }
